ce18db9eb24bb657516a35ec58e75b261f75fd0a
s7.nakamura <s7.nakamura@gmail.com>
2019-06-08 15:11:04 +0900

    手動でmasterと合わせてみる

--- jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
+++ jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
@@ -3,11 +3,12 @@ package jp.co.nri.nefs.tool.smartlauncher.gui;
 public class GitTest {
 
 	public void Test(){
+		System.out.println("master");
 		System.out.println("feature");
 		System.out.println("master2");
 		System.out.println("feature2");
 		System.out.println("master3");
 		System.out.println("master4");
-		System.out.println("feature3");
+		System.out.println("master5");
 	}
 }
