2019b13d3c30a7f951b44589534c65ccdcbcf6dc
s7.nakamura <s7.nakamura@gmail.com>
2019-06-08 15:24:02 +0900

    revert

--- jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
+++ jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
@@ -3,7 +3,6 @@ package jp.co.nri.nefs.tool.smartlauncher.gui;
 public class GitTest {
 
 	public void Test(){
-		System.out.println("master");
 		System.out.println("feature");
 		System.out.println("master2");
 		System.out.println("feature2");
