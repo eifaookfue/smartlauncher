cb827af69acd57e6a36ef232e4e6f35b6cd5c560
s7.nakamura <s7.nakamura@gmail.com>
2019-06-08 15:29:40 +0900

    aaa
    Conflicts:
    	jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java

--- jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
+++ jp.co.nri.nefs.tool.smartlauncher/src/main/java/jp/co/nri/nefs/tool/smartlauncher/gui/GitTest.java
@@ -3,12 +3,13 @@ package jp.co.nri.nefs.tool.smartlauncher.gui;
 public class GitTest {
 
 	public void Test(){
-		System.out.println("master");
 		System.out.println("feature");
 		System.out.println("master2");
 		System.out.println("feature2");
 		System.out.println("master3");
 		System.out.println("master4");
 		System.out.println("master5");
+		System.out.println("feature3");
+		System.out.println("feature4");
 	}
 }
